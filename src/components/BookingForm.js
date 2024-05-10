b5268885e0dd99dbfa9c63191f1895284e343d2a
TKLahtinen <n2lati00@students.oamk.fi>
2024-05-10 11:27:50 +0300

    Unit testing

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -2,10 +2,12 @@
 import { useEffect, useState } from 'react'
 
 const BookingForm = ( {availableTimes, dispatchOnDateChange, submitData} ) => {
+  const minimumDate = new Date().toISOString().split('T')[0];
+  const defaultTime = availableTimes[0];
   const occasions = ['Birthday', 'Anniversary']
-  const [date, setDate] = useState(undefined)
-  const [resTime, setResTime] = useState('17:00')
-  const [guests, setGuests] = useState('')
+  const [date, setDate] = useState(minimumDate)
+  const [resTime, setResTime] = useState(availableTimes[0])
+  const [guests, setGuests] = useState(1)
   const [occasion, setOccasion] = useState('Birthday')
 
 // error states
@@ -19,7 +21,7 @@ const BookingForm = ( {availableTimes, dispatchOnDateChange, submitData} ) => {
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    if(date === undefined){
+    if(date === ''){
       setDateError(true);
     }
     else if(guests === '' || guests < 1 || guests > 10) {
@@ -27,7 +29,7 @@ const BookingForm = ( {availableTimes, dispatchOnDateChange, submitData} ) => {
       setGuestsError(true);
     }
     else {
-    submitData(e)
+    submitData({date, resTime, guests, occasion})
     }
   }
 
@@ -35,25 +37,41 @@ const BookingForm = ( {availableTimes, dispatchOnDateChange, submitData} ) => {
     <div className='form-container'>
       <h2>Book a table</h2>
       <form>
-          <label htmlFor="res-date">Choose date</label>
-          <input type="date" id="res-date" onChange={handleDateChange} className={dateError ? 'error' : ''} />
-          {dateError && <p className='error-text'>Please select a date</p>}
-          <label htmlFor="res-time">Choose time</label>
+          <label htmlFor="res-date">
+            Choose date
+          </label>
+          <input type="date" id="res-date" onChange={handleDateChange}  value={date} className={dateError ? 'error' : ''} />
+          {dateError &&
+          <p className='error-text' data-testid='date-error-text'>
+          Please select a date
+          </p>}
+          <label htmlFor="res-time">
+            Choose time
+          </label>
           <select id="res-time" onChange={((e) => setResTime(e.target.value))}>
             {availableTimes.map((time, index) => (
-            <option key={index} value={time}>{time}</option>
+            <option data-testid="booking-time-option" key={index} value={time}>{time}</option>
             ))}
           </select>
-          <label htmlFor="guests">Number of guests</label>
-          <input type="number" placeholder="Select how many guests are with you." min="1" max="10" id="guests" onChange={((e) => setGuests(e.target.value))} className={guestsError ? 'error' : ''}/>
-          {guestsError && <p className='error-text'>Please select a number of guests between 1 and 10</p>}
-          <label htmlFor="occasion">Occasion</label>
+          <label htmlFor="guests">
+            Number of guests
+          </label>
+          <input type="number" value={guests} min="1" max="10" id="guests" onChange={((e) => setGuests(e.target.value))} className={guestsError ? 'error' : ''}/>
+          {guestsError &&
+          <p className='error-text' data-testid='error-text'>
+            Please select a number of guests between 1 and 10
+          </p>}
+          <label htmlFor="occasion">
+            Occasion
+          </label>
           <select id="occasion"  onChange={((e) => setOccasion(e.target.value))}>
               {occasions.map((choice, index) => (
-                  <option key={index}>{choice}</option>
+                  <option data-testid='occasion-option' key={index}>{choice}</option>
               ))}
           </select>
-          <button type="submit" onClick={handleSubmit} className='btn-reserve'>Make your reservation!</button>
+          <button type="submit" onClick={handleSubmit} className='btn-reserve'>
+            Make your reservation!
+          </button>
       </form>
     </div>
   )
--- src/components/BookingForm.test.js
+++ src/components/BookingForm.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import BookingForm from "./BookingForm";
 
 describe('Booking form', () => {
@@ -7,11 +7,103 @@ describe('Booking form', () => {
     const dispatchOnDateChange = jest.fn();
     const submitData = jest.fn();
 
-test('Renders a choose date label', () => {
-    render(<BookingForm availableTimes={availableTimes}/>)
-    const dateLabel = screen.getByText("Choose date")
-    expect(dateLabel).toBeInTheDocument()
+test ('renders the form with default values', async () => {
+    render(
+        <BookingForm
+            availableTimes={availableTimes}
+            submitData={submitData}
+        />
+    )
+
+    const dateInput = screen.getByLabelText(/Choose date/);
+    const timeSelect = screen.getByLabelText(/Choose time/);
+    const timeOptions = await screen.findAllByTestId('booking-time-option');
+    const numberOfGuestsInput = screen.getByLabelText(/Number of guests/);
+    const occasionSelect = screen.getByLabelText(/Occasion/);
+    const occasionOptions = await screen.findAllByTestId('occasion-option');
+    const submitButton = screen.getByRole('button')
+
+    expect(dateInput).toBeInTheDocument();
+    expect(dateInput).toHaveAttribute('type', 'date');
+    expect(dateInput).toHaveAttribute('id', 'res-date');
+    expect(dateInput).toHaveValue(today);
+
+    expect(timeSelect).toBeInTheDocument();
+    expect(timeSelect).toHaveAttribute('id', 'res-time');
+    expect(timeOptions.length).toBe(2);
+
+    expect(numberOfGuestsInput).toBeInTheDocument();
+    expect(numberOfGuestsInput).toHaveAttribute('type', 'number');
+    expect(numberOfGuestsInput).toHaveAttribute('id', 'guests');
+    expect(numberOfGuestsInput).toHaveAttribute('min', '1');
+    expect(numberOfGuestsInput).toHaveAttribute('max', '10');
+    expect(numberOfGuestsInput).toHaveValue(1);
+
+    expect(occasionSelect).toBeInTheDocument();
+    expect(occasionSelect).toHaveAttribute('id', 'occasion');
+    expect(occasionOptions.length).toBe(2);
+
+    expect(submitButton).toBeInTheDocument();
+    expect(submitButton).toHaveAttribute('type', 'submit');
 })
 
+    test('successfully submits the form with valid data', () => {
+        render(
+            <BookingForm
+                availableTimes={availableTimes}
+                submitData={submitData}
+            />
+        )
+
+        const submitButton = screen.getByRole('button');
+        fireEvent.click(submitButton);
+
+        expect(submitData).toHaveBeenCalledWith({
+            date: today,
+            resTime: availableTimes[0],
+            guests: 1,
+            occasion: 'Birthday'
+        })
+    })
+
+    test('shows an error message when date is not selected', () => {
+        render(
+            <BookingForm
+                availableTimes={availableTimes}
+                submitData={submitData}
+                dispatchOnDateChange={dispatchOnDateChange}
+            />
+        )
+
+        const submitButton = screen.getByRole('button');
+        const dateInput = screen.getByLabelText(/Choose date/);
+        
+        fireEvent.change(dateInput, { target: { value: '' } });
+        fireEvent.blur(dateInput);
+        fireEvent.click(submitButton);
+
+        const errorMessage = screen.getByText(/Please select a date/);
+        expect(errorMessage).toHaveTextContent('Please select a date');
+    })
+
+    test('shows an error message when number of guests is not selected', () => {
+        render(
+            <BookingForm
+                availableTimes={availableTimes}
+                submitData={submitData}
+                dispatchOnDateChange={dispatchOnDateChange}
+            />
+        )
+
+        const submitButton = screen.getByRole('button');
+        const numberOfGuestsInput = screen.getByLabelText(/Number of guests/);
+
+        fireEvent.change(numberOfGuestsInput, { target: { value: '' } });
+        fireEvent.blur(numberOfGuestsInput);
+        fireEvent.click(submitButton);
+
+        const errorMessage = screen.getByText(/Please select a number of guests between 1 and 10/);
+        expect(errorMessage).toHaveTextContent('Please select a number of guests between 1 and 10');
+    })
 })
 
